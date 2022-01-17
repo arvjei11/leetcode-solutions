ca0e2793a8af9b3371cdea91a64ef364f7483390
arvjei11 <50538140+arvjei11@users.noreply.github.com>
2022-01-17 14:12:19 +0530

    added fibonnaci number

---
+++ Java/Fibonnaci_Number.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int fib(int n) { 
+        int[] arr = new int[n+2];arr[0]= 0;arr[1]= 1;
+        for(int i =2;i<=n;i++)
+            arr[i] = arr[i-1]+arr[i-2];
+        return arr[n];    
+    }
+}
